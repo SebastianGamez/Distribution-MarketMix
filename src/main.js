88ca6ef5612b2632a0615e2e0b22342be46cc3e8
juans <juansmgb@gmail.com>
2023-02-07 20:12:15 -0500

    añadicion del archivo js de la aplicacion

---
+++ src/main.js
@@ -0,0 +1,56 @@
+
+
+
+let app = new Vue({
+    el: "#app",
+    data: {
+        bodegaUno: 100000,
+        bodegaDos: 230000
+    },
+    methods: {
+        compraBodegaUno() {
+            let cantidad = document.getElementById("inputCantidad").value;
+            let tipoPeso = document.getElementById("tipoPeso").value;
+            let cantidadKilos = 0;
+
+            if(tipoPeso == 1) {
+                cantidadKilos = cantidad;
+            } else if(tipoPeso == 2) {
+                cantidadKilos = cantidad * 0.453592;
+            } else if(tipoPeso == 3) {
+                cantidadKilos = cantidad / 1000;
+            }
+
+            if(this.bodegaUno - cantidadKilos > this.bodegaUno / 2) {
+                alert("Compra realizada con exito!");
+                this.bodegaUno -= cantidadKilos;
+            } else if(this.bodegaUno - cantidadKilos < this.bodegaUno / 10) {
+                alert("No queda suficiente arroz en esta bodega, por favor elija otra bodega");
+            } else {
+                alert("Quedan menos de la mitad de arroz en esta bodega, por favor elija otra bodega");
+            }
+        },
+        compraBodegaDos() {
+            let cantidad = document.getElementById("inputCantidad").value;
+            let tipoPeso = document.getElementById("tipoPeso").value;
+            let cantidadKilos = 0;
+
+            if(tipoPeso == 1) {
+                cantidadKilos = cantidad;
+            } else if(tipoPeso == 2) {
+                cantidadKilos = cantidad * 0.453592;
+            } else if(tipoPeso == 3) {
+                cantidadKilos = cantidad / 1000;
+            }
+
+            if(this.bodegaDos - cantidadKilos > this.bodegaDos / 2) {
+                alert("Compra realizada con exito!");
+                this.bodegaDos -= cantidadKilos;
+            } else if(this.bodegaDos - cantidadKilos < this.bodegaDos / 10) {
+                alert("No queda suficiente arroz en esta bodega, por favor elija otra bodega");
+            } else {
+                alert("Quedan menos de la mitad de arroz en esta bodega, por favor elija otra bodega");
+            }
+        }
+    }
+});
