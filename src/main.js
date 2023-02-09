22e683a226a50762eab93525d14f936ec4840a6d
Sebastian Gamez Ariza <juan.gamez1001@gmail.com>
2023-02-08 21:40:58 -0500

    Se añaden los registros

--- src/main.js
+++ src/main.js
@@ -4,12 +4,36 @@ const { createApp } = Vue;
 const app = createApp({
     data (){
         return {
+            render: {
+                verRegistros: false,
+                registros: false,
+                compra: true
+            },
+            
             cantidad: '',
             tipoPeso: '',
             bodegaUno: 100000,
-            bodegaDos: 230000
+            bodegaDos: 230000,
+
+            registros: []
         }
     },
+
+
+
+    mounted() {
+        this.registros = JSON.parse(localStorage.getItem("registros"))? JSON.parse(localStorage.getItem("registros")) : [];
+        if(this.registros.length > 0) {
+            this.render.verRegistros = true;
+        }
+    },
+
+    updated() {
+        if(this.registros.length > 0) {
+            this.render.verRegistros = true;
+        }
+    },
+
     methods: {
         compraBodegaUno() {
             let cantidad = this.cantidad;
@@ -29,6 +53,12 @@ const app = createApp({
             else if(this.bodegaUno - cantidadKilos > this.bodegaUno / 2) {
                 alert("Compra realizada con exito!");
                 this.bodegaUno -= cantidadKilos;
+                this.registros.push({
+                    bodega: 1,
+                    cantidad: cantidadKilos,
+                });
+                localStorage.setItem("registros", JSON.stringify(this.registros));
+                this.cantidad = '';
             } else if(this.bodegaUno - cantidadKilos < this.bodegaUno / 10) {
                 alert("No queda suficiente arroz en esta bodega, por favor elija otra bodega");
             } else {
@@ -53,11 +83,25 @@ const app = createApp({
             else if(this.bodegaDos - cantidadKilos > this.bodegaDos / 2) {
                 alert("Compra realizada con exito!");
                 this.bodegaDos -= cantidadKilos;
+                this.registros.push({
+                    bodega: 2,
+                    cantidad: cantidadKilos,
+                });
+                localStorage.setItem("registros", JSON.stringify(this.registros));
+                this.cantidad = '';
             } else if(this.bodegaDos - cantidadKilos < this.bodegaDos / 10) {
                 alert("No queda suficiente arroz en esta bodega, por favor elija otra bodega");
             } else {
                 alert("Quedan menos de la mitad de arroz en esta bodega, por favor elija otra bodega");
             }
+        },
+        obtenerRegistros() {
+            this.render.registros = true;
+            this.render.compra = false;
+        },
+        volver() {
+            this.render.registros = false;
+            this.render.compra = true;
         }
     }
 });
